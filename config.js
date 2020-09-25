ca693e28f808333e78588bc6a87efd82da0da617
arvind pamuri <aravind.pamuri@gmail.com>
2020-09-25 09:47:04 +0530

    OAuth with passport facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '798762620952050',
+        clientSecret: '56d820fefbfd8e94e64a143e50a56b0b'
+    }
 }
